b201080179f18fd4ce82c2cf777b465ddc7759f2
mirenk0 <mirenkodaci21@gmail.com>
2025-10-09 15:43:25 +0200

    Update fav

--- quartz/plugins/emitters/favicon.ts
+++ quartz/plugins/emitters/favicon.ts
@@ -13,7 +13,7 @@ export const Favicon: QuartzEmitterPlugin = () => ({
 
     yield write({
       ctx: { argv } as BuildCtx,
-      slug: "favicon-v2" as FullSlug,
+      slug: "favicon" as FullSlug,
       ext: ".ico",
       content: faviconContent,
     })
